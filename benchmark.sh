358cd804d1125e2e1bc276dc936c078040bf7a00
Geoffrey Hannigan <ghanni@upenn.edu>
2016-06-28 07:07:11 -0400

    Added larger file for benchmarking.

--- benchmark.sh
+++ benchmark.sh
@@ -2,8 +2,8 @@
 # Some quick benchmarking to test out Julia while
 # and comparing to Perl.
 
-for itr in 100 1000 10000 50000 100000 250000 472897; do
-	head -n ${itr} uniprot-bacteriophage-2.fasta > ./tmpseq.fa
+for itr in 100 1000 10000 50000 100000 250000 500000 1000000 1500000 5000000 10000000 15000000 20000000 25000000 40000000; do
+	head -n ${itr} largeseqtest.fa > ./tmpseq.fa
 	{ time perl calculate_fasta_median_length.pl ./tmpseq.fa; } 2> ./tmpPerlTime${itr}.log
 	grep [a-zA-Z0-9] ./tmpPerlTime${itr}.log | awk -v var=$itr '{ print $0"\tPerl\t"var }' | sed 's/.m\(.\)./\1/' | sed 's/\([0-9]\)s/\1/' > ./tmpPerlTime${itr}form.log
 	{ time julia calculate_fasta_median_length.jl ./tmpseq.fa; } 2>> ./tmpJuliaTime${itr}.log
